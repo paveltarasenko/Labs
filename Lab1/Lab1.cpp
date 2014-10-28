6554f94f31171f700e8fd64492ba7e801a317132
Tarasenko Pavlo <pavel.tarasenko@wonga.com>
2014-10-28 17:45:56 +0200

    first

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,21 @@
+﻿#include "stdafx.h"
+#include <iostream>
+#include <stack>
+#include <stdio.h>
+
+using namespace std;
+
+
+
+void main()
+{
+ 
+
+	 cout<<" Hello";
+
+	
+ char name[50];
+ 
+ cin >> name;
+   
+}
